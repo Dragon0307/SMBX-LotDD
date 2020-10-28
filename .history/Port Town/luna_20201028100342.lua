b5d0845d7b8ffae44a9e2d36554fb95686af0568
Dragon0307 <dragon0307.galeem@gmail.com>
2020-10-28 12:19:56 +0000

    Replaced Kero Sewers and Port Town with older, more finished levels that I made on a different device..

---
+++ .history/Port Town/luna_20201028100342.lua
@@ -0,0 +1,27 @@
+--------------------------------------------------
+-- Level code
+-- Created 9:59 2020-10-28
+--------------------------------------------------
+SaveData[Level.filename()] = SaveData[Level.filename()] or {}
+local LevelSD = SaveData[Level.filename()]
+
+-- Run code on level start
+function onStart()
+    --Your code here
+end
+
+-- Run code every frame (~1/65 second)
+-- (code will be executed before game logic will be processed)
+function onTick()
+    --Your code here
+end
+
+-- Run code when internal event of the SMBX Engine has been triggered
+-- eventName - name of triggered event
+function onEvent(eventName)
+    if eventName == "Toad Quest"
+        if LevelSD.motorQuest = false then
+        Text.showMessageBox("...or I WOULD, if the Wario Bros. didn't steal my motor! It's been locked aw")
+end
+
+
