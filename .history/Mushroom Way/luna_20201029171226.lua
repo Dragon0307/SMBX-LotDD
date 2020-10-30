a4665966308555712beec07e1ff0f4fd59206ca5
Dragon0307 <dragon0307.galeem@gmail.com>
2020-10-30 08:43:13 +0000

    Didi some stuff

---
+++ .history/Mushroom Way/luna_20201029171226.lua
@@ -0,0 +1,24 @@
+local warpTransition = require("warpTransition")
+warpTransition.crossSectionTransition = warpTransition.
+
+soundPlayed = 0
+
+function onEvent(event)
+    if event == "Hammer Bro Music" then
+        Audio.MusicChange(0, "Mid-Boss - Super Mario Sunshine.ogg")
+    elseif event == "Hammer Bro Killed" then
+        Audio.MusicChange(0, "mrpg-mushroompass.spc")
+    elseif event == "Stop Music" then
+        Audio.MusicChange(2, 0)
+    elseif event == "Lift Start" and soundPlayed == 0 then
+        Audio.playSFX("Sounds/nsmbwiiBoneRollercoasterActivate.ogg")
+        soundPlayed = 1
+    end
+end
+
+stats.xpDrop(89, 1)
+stats.xpDrop(173, 1)
+stats.xpDrop(29, 2)
+stats.xpDrop(27, 1)
+stats.xpDrop(28, 1)
+stats.xpDrop(233, 1)
