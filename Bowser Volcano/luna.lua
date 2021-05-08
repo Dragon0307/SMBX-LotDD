570ac9be7ae07723b588f51dadbaedbb3ad4c158
ShadowXeldron <dragon0307.galeem@gmail.com>
2021-05-08 11:37:14 +0100

    1K+ commits oh god I never even did anything what the hell

---
+++ Bowser Volcano/luna.lua
@@ -0,0 +1,34 @@
+--[[
+
+Midboss: Blarg 
+Theme - SMS Midboss Theme
+Reused from Bowser's Volcano, but more difficult now due to the fact it is too hot to use Chill Out in the volcano.
+
+Boss: Big Bad Barry
+Theme - Maximum Overdrive
+A giant Blarg who claims to own the Star Shard. He likes fire - a lot. He is the last boss before Wario is defeated and the Hardmode begins.
+
+
+]]
+
+--[[ Sniffit of Whiffit
+
+Snifft: Ladies and Gentlesniffits! Today we have a special guest on Sniffit or Whiffit: Lava Pit Edition - Mr. Mario "Super Mario" Mario!
+Mario: What-a-bout-a the free-a Robux from that-a block?
+Sniffit: What Robux? I haven't a clue what you're talking about. But if it's free, then it's probably too good to be true. Anyway, welcome to Sniffit or Whiffit!
+Sniffit: See, if you do well you'll win points. If you get enough, you'll win today's grand prize: The Obsidian Boots! These are special shoes that are all black and shiny,
+    and not to mention, unpractical! You can't run with them on and don't even think of trying to sprint. I guess they could let you stand in lava, but they don't fit on my feet
+    so I haven't tried. (First try)
+Sniffit: See, if you do well you'll win points. If you get enough, you'll win today's grand prize: The Casino Card! This card is made of solid gold, so it paints a big "mug me" 
+    sign over your head. Not to mention, it is also an exclusive card from our sponsor: The Shy Guy Mafia's Dodgy Casino! Ever wanted to do boring shoehorned minigames which
+    directly fund organized crime? Well now you can! (Second try)
+Sniffit: And, if you get any wrong, you'll lose a life. If you run out of lives... well, you'll get a nice, warm bath.
+
+Correct! Next question.
+Oops! You got that wrong! Lose a life.
+Uh-oh! Seems you're out of lives! Thank you for playing, and we'll try to fish out your charred rem,ains later today. Probably.
+Well, it seems you got everything right! As promises, here's your prize.
+Wow, you got everything right! Here - take the grand prize for the day: The Fire Emblem! It's a shield that lets you shoot fireballs anywhere - even underwater! No, it doesn't 
+protect against anything. Dragon killing sword not included.
+
+]]
